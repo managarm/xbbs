40f1b1035389f295fbb729de75745d8bd17b6503
Arsen Arsenović <arsen@aarsen.me>
2020-08-27 13:26:33 +0200

    clean up project structure

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-from setuptools import setup
-import os.path as path
-
-reqs = path.join(path.dirname(__file__), "requirements.txt")
-requirements = []
-with open(reqs) as f:
-    for line in f:
-        req = line.split('#')[0].strip()
-        if len(req) > 0:
-            requirements.append(req)
+from setuptools import setup, find_packages
 
 setup(
     name="xbci",
@@ -15,14 +6,8 @@ setup(
     description="A dependency-resolving distributed CI system for xbstrap",
     author="Arsen Arsenovic",
     author_email="arsen@aarsen.me",
-    packages=[
-        "xbci",
-        "xbci.worker",
-        "xbci.coordinator",
-        "xbci.protocol"
-    ],
+    packages=find_packages(),
     license="AGPL-3.0-only",
-    # TODO(arsen): remove requirements file
     install_requires=[
         "attrs",
         "gunicorn",
