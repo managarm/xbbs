c5e358faf017242ef4ab9331d0e5ae7c46a2745d
Arsen Arsenović <arsen@aarsen.me>
2020-12-23 23:43:05 +0100

    messages: correct mistake in last commit

--- xbbs/messages.py
+++ xbbs/messages.py
@@ -144,7 +144,7 @@ class JobMessage(BaseMessage):
     prod_files = attr.ib()
     tool_repo = attr.ib()
     pkg_repo = attr.ib()
-    rolling_ids = attr.ib()
+    commits_object = attr.ib()
     # XXX: maybe it's worth doing something else
     xbps_keys = attr.ib(default=None, repr=False)
     _validator = V.parse({
