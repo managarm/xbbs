54335dcee8b362c99cd0318064b49fef2e2014f2
Arsen Arsenović <arsen@managarm.org>
2025-07-16 14:28:07 +0200

    meta: add a marker .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+;; Empty.  Used to tell Emacs to read .dir-locals-2.el (bug?)
+()
