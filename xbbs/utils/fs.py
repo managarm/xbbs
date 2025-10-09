99fc07e57bb6c9dfeab16089c472f163979c32b8
Arsen Arsenović <arsen@managarm.org>
2025-10-09 11:50:44 +0200

    utils/fs: fix typo in docstring
    I think I was writing too much Java or something.

--- xbbs/utils/fs.py
+++ xbbs/utils/fs.py
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-This package contains utilities used for dealing with the filesystem.
+This module contains utilities used for dealing with the filesystem.
 """
 
 import contextlib
